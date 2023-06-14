46e43a7f5365ae9fe8eb6fea71ba9fd55437e859
Casey Sprague <email@caseysprague.com>
2023-06-14 15:44:23 -0500

    Replace OmgSamesies with Contract Cardinal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
                     </div>
                     <h2 class="mb-6 space-y-6 sm:space-y-4 md:space-y-2">
                         <p class="font-light text-xl sm:text-2xl text-gray-400 leading-normal">Developer, entrepreneur, investor. Scrum pilot. Actual pilot.</p>
-                        <p class="font-light text-base text-gray-400 leading-normal">Co-Founder, <a class="text-blue-400 hover:text-blue-500 no-underline" href="https://reservoirwave.com">ReservoirWave</a> and OmgSamesies. Previously CTO & Technical Co-Founder, <a class="text-blue-400 hover:text-blue-500 no-underline" href="https://www.thera-link.com">thera-LINK</a>.</p>
+                        <p class="font-light text-base text-gray-400 leading-normal">Co-Founder, <a class="text-blue-400 hover:text-blue-500 no-underline" href="https://reservoirwave.com">ReservoirWave</a> and <a class="text-blue-400 hover:text-blue-500 no-underline" href="https://contractcardinal.com">Contract Cardinal</a>. Previously CTO & Technical Co-Founder, <a class="text-blue-400 hover:text-blue-500 no-underline" href="https://www.thera-link.com">thera-LINK</a>.</p>
                     </h2>
                     <div class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
                         @if($showBlog)
