76631e35c74d6fd805cb54087710fc8e12232b90
Casey Sprague <email@caseysprague.com>
2024-04-19 13:05:15 -0500

    Change "scrum pilot" to "code pilot"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
                         @include('includes.logo')
                     </div>
                     <h2 class="mb-6 space-y-6 sm:space-y-4 md:space-y-2">
-                        <p class="font-light text-xl sm:text-2xl text-gray-400 leading-normal">Developer, entrepreneur, investor. Scrum pilot. Actual pilot.</p>
+                        <p class="font-light text-xl sm:text-2xl text-gray-400 leading-normal">Developer, entrepreneur, investor. Code pilot. Actual pilot.</p>
                         <p class="font-light text-base text-gray-400 leading-normal">Co-Founder, <a class="text-blue-400 hover:text-blue-500 no-underline" href="https://reservoirwave.com">ReservoirWave</a> and <a class="text-blue-400 hover:text-blue-500 no-underline" href="https://contractcardinal.com">Contract Cardinal</a>. Previously CTO & Technical Co-Founder, <a class="text-blue-400 hover:text-blue-500 no-underline" href="https://www.thera-link.com">thera-LINK</a>.</p>
                     </h2>
                     <div class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
